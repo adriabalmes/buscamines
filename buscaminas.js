0dfccf35f09dcca5f62757bdece6a9317c405a0b
adriabalmes <aberruez@jaumebalmes.net>
2022-12-12 21:51:27 +0100

    buscaminas parte final

--- buscaminas.js
+++ buscaminas.js
@@ -6,8 +6,15 @@ let minas = [];
 let banderas = [];
 // variable minas repartidas
 let minasRepartidas = 0;
+// nombre del jugador para registrar
+let nombreGanador = "";
+// variables para temporizador()
+var minutos = 0;
+var segundos = 0;
+var tiempo;
+let puntuacionGanador;
 
-
+// funcion para crear la tabla
 function createTable () {
     let rows = document.getElementById("inputX").valueAsNumber;
     let columns = document.getElementById("inputY").valueAsNumber;
@@ -41,36 +48,13 @@ function createTable () {
     table.appendChild(tbody);
     div.appendChild(table);
     // tabla creada en matrix
-    rows = document.querySelector("tbody").children
-    for (var i = 0; i < rows.length; i++) {
-        matrix.push(rows[i].children);
-    }
-    // inicializamos la matriz todo a 0
-    for (let i = 0; i < columns; i++) {
-        minas[i] = [];
-        for (let j = 0; j < rows; j++) {
-            minas[i][j] = 0;
-        }
+    let row = document.querySelector("tbody").children;
+    for (var i = 0; i < row.length; i++) {
+        matrix.push(row[i].children);
     }
 }
 
-// function matriuBinaria(matrix) {
-//     let matrix2 = []
-//     for (var i = 0; i < matrix.length; i++) {
-//         let rows = [];
-//         for (var j = 0; j < matrix[0].length; j++) {
-//             if (matrix[i][j].style.backgroundColor == "red") {
-//                 rows.push(1);
-//             }
-//             else {
-//                 rows.push(0);
-//             }
-//         }
-//         matrix2.push(rows); 
-//     }
-//     return matrix2;
-// }
-
+// funcion para inicializar minas
 function inicialitzaMines() {
     let rows = document.getElementById("inputX").valueAsNumber;
     let columns = document.getElementById("inputY").valueAsNumber;
@@ -86,10 +70,10 @@ function inicialitzaMines() {
         }
         // repartimos minas en matriz minas[]
         while (minasRepartidas < nMinas) {
-            let fila = Math.floor(Math.random() * rows);
-            let columna = Math.floor(Math.random() * columns);
-            if (minas[fila][columna] == 0){
-                minas[fila][columna] = 1;
+            let fila = Math.floor(Math.random() * (rows));
+            let columna = Math.floor(Math.random() * (columns));
+            if (minas[columna][fila] == 0){
+                minas[columna][fila] = 1;
                 //contador para minas repartidas
                 minasRepartidas++;
             }
@@ -111,10 +95,10 @@ function inicialitzaMines() {
             }
         }
     }
+    temporizador();
 }
 
-
-
+// funcion para inicialiar juego 
 function inicializaJuego() {
     createTable ();
     let rows = document.getElementById("inputX").valueAsNumber;
@@ -133,8 +117,8 @@ function inicializaJuego() {
         while (minasRepartidas < nMinas){
             let fila = Math.floor(Math.random() * rows);
             let columna = Math.floor(Math.random() * columns);
-            if (minas[fila][columna] == 0){
-                minas[fila][columna] = 1;
+            if (minas[columna][fila] == 0){
+                minas[columna][fila] = 1;
                 //contador para minas repartidas
                 minasRepartidas++;
             }
@@ -147,20 +131,10 @@ function inicializaJuego() {
             }
         }
     }
+    temporizador();
 }
 
-// function coordenadaCelda(event) {
-//     if (event.target.tagName == "TD") {
-//         console.log(event.target.id);
-//     }
-//     if (minas[event.target.className][event.target.id] == 1) {
-//         return console.log("HAY MINA");
-//     }
-//     else {
-//         return console.log("NO HAY MINA");
-//     }
-// }
-
+// funcion para pintar minas alrededor
 function paintAllNeighbours() {
     let count;
     for (let i = 0; i < matrix.length; i++ ) { 
@@ -173,6 +147,7 @@ function paintAllNeighbours() {
     }
 } 
 
+// funcion para contar minas de alrededor
 function countNeighbours(x, y) {
     let count = 0;
     for (let i = y - 1; i <= y + 1; i++) { // Y
@@ -191,7 +166,7 @@ function countNeighbours(x, y) {
     return count;
 }
 
-
+// funcion para destapar minas de alrededor
 function minasAlrededor(event) { 
     if (event.target.tagName == "TD") {
         let inputX = parseInt(event.target.id);
@@ -204,7 +179,6 @@ function minasAlrededor(event) {
                      if (banderas[i][j] != "B") {
                         matrix[i][j].innerText = count;
                         matrix[i][j].style.backgroundColor = "whitesmoke";
-                        casillasDestapadas++;
                         if (matrix[i][j].innerText == 1) {
                             matrix[i][j].style.color = "blue";
                         }
@@ -229,6 +203,7 @@ function minasAlrededor(event) {
     hasPerdido();
 }
 
+// funcion para insertar bandera
 function poneBandera(event) {
     let bandera = document.createElement('img');
     bandera.src  = 'img/bandera.png';
@@ -245,8 +220,10 @@ function poneBandera(event) {
         matrix[inputY][inputX].appendChild(bandera);
         banderas[inputY][inputX] = "B";
     }
+    hasGanado();
 }
 
+// funcion para insertar bomba
 function bomba() {
     let bomba = document.createElement('img');
         bomba.src  = 'img/bomba.png';
@@ -256,7 +233,7 @@ function bomba() {
         return bomba;
 }
 
-
+// funcion para determinar que has ganado
 function hasGanado() {
     let hasGanado = document.getElementById("hasGanado");
     let juegoTerminado = true;
@@ -272,9 +249,13 @@ function hasGanado() {
         hasGanado.style.display = "block";
         paintAllNeighbours();
         desactivaJuegoGanado();
+        desactivaJuegoGanadoDesplegable();
+        clearInterval(tiempo);
+        registroGanador();
     }
 }
 
+// funcion para desactivar el juego cuando has ganado
 function desactivaJuegoGanado() {
     let rows = document.getElementById("inputX").valueAsNumber;
     let columns = document.getElementById("inputY").valueAsNumber;
@@ -300,6 +281,7 @@ function desactivaJuegoGanado() {
     }
 }
 
+// función que determina que has perdido
 function hasPerdido() {
     let inputX = parseInt(event.target.id);
     let inputY = parseInt(event.target.className);
@@ -308,9 +290,12 @@ function hasPerdido() {
         hasPerdido.style.display = "block";
         paintAllNeighbours();
         desactivaJuegoPerdido();
+        desactivaJuegoPerdidoDesplegable();
+        clearInterval(tiempo);
     }
 }
 
+// funcion para desactivar el juego cuando has perdido
 function desactivaJuegoPerdido() {
     let rows = document.getElementById("inputX").valueAsNumber;
     let columns = document.getElementById("inputY").valueAsNumber;
@@ -332,4 +317,177 @@ function desactivaJuegoPerdido() {
             }
         }
     }
+}
+
+// funcion para crear el juego desplegable
+function juegoDesplegable() {
+    let valores = document.getElementById("predeterminados").value.split(":")
+    let inputX = parseInt(valores[0]);
+    let inputY = parseInt(valores[1]);
+    let nMinas = parseInt(valores[2]);
+
+    //CREAMOS TABLA createTable()
+    let rows = inputX;
+    let columns = inputY;
+    let div = document.getElementById("idDIV");
+    let table = document.createElement("table");
+    let tbody = document.createElement("tbody");
+        // creamos la tabla
+        for (let i = 0; i < columns; i++) {
+            let fila = document.createElement("tr"); // se crea la FILA
+            for (let j = 0; j < rows; j++) {
+                let celda = document.createElement("td");  // se crea la CELDA
+                celda.style.border = "2px solid dimgray";
+                celda.style.width = "40px";
+                celda.style.height = "40px";
+                celda.style.paddingLeft = "0";
+                celda.style.paddingTop = "0";
+                celda.style.paddingRight = "0";
+                celda.style.paddingBottom = "0";
+                celda.style.textAlign = "center";
+                celda.style.fontWeight = "bold";
+                celda.style.fontSize = "1.2em";
+                celda.style.backgroundColor = "lightgrey";
+                celda.setAttribute("id", j); // asginamos coordenada X en ID
+                celda.setAttribute("class", i); // asginamos coordenada Y en CLASS
+                celda.addEventListener("click", minasAlrededor); // asignamos funcion CLICK en celda
+                celda.addEventListener("contextmenu", poneBandera); // asignamos boton DCH en celda para bandera
+                fila.appendChild(celda);
+            }
+            tbody.appendChild(fila);
+        }
+        table.appendChild(tbody);
+        div.appendChild(table);
+        // tabla creada en matrix
+        let row = document.querySelector("tbody").children;
+        for (var i = 0; i < row.length; i++) {
+            matrix.push(row[i].children);
+        }
+  
+        // inicializamos la matriz minas todo a 0
+        for (let i = 0; i < columns; i++) {
+            minas[i] = [];
+            for (let j = 0; j < rows; j++) {
+                minas[i][j] = 0;
+            }
+        }
+        // inicializamos la matriz banderas[] toda VACÍA
+        for (let i = 0; i < columns; i++) {
+            banderas[i] = [];
+            for (let j = 0; j < rows; j++) {
+                banderas[i][j] = "";
+            }
+        }
+        // repartimos minas en matriz minas[]
+        while (minasRepartidas < nMinas) {
+            let fila = Math.floor(Math.random() * rows);
+            let columna = Math.floor(Math.random() * columns);
+            if (minas[columna][fila] == 0){
+                minas[columna][fila] = 1;
+                //contador para minas repartidas
+                minasRepartidas++;
+            }
+        }
+    temporizador();
+}
+
+// funcion que desactiva el juego desplegable cuando has perdido
+function desactivaJuegoPerdidoDesplegable() {
+    let valores = document.getElementById("predeterminados").value.split(":")
+    let inputX = parseInt(valores[0]);
+    let inputY = parseInt(valores[1]);
+    let rows = inputX;
+    let columns = inputY;
+     // pintamos las minas en la tabla matrix[]
+     for (let i = 0; i < columns; i++) {
+        for (let j = 0; j < rows; j++) {
+            if (minas[i][j] == 1) {
+                matrix[i][j].style.backgroundColor = "red";
+                matrix[i][j].innerText = "";
+                matrix[i][j].removeEventListener("click", minasAlrededor, false); 
+                matrix[i][j].removeEventListener("contextmenu", poneBandera, false); 
+                matrix[i][j].appendChild(bomba());
+            }
+            else {
+                matrix[i][j].style.backgroundColor = "black";
+                matrix[i][j].style.color = "white";
+                matrix[i][j].removeEventListener("click", minasAlrededor, false); 
+                matrix[i][j].removeEventListener("contextmenu", poneBandera, false); 
+            }
+        }
+    }
+}
+
+// funcion que desactiva el juego desplegable cuando has ganado
+function desactivaJuegoGanadoDesplegable() {
+    let valores = document.getElementById("predeterminados").value.split(":")
+    let inputX = parseInt(valores[0]);
+    let inputY = parseInt(valores[1]);
+    let rows = inputX;
+    let columns = inputY;
+     // pintamos las minas en la tabla matrix[]
+     for (let i = 0; i < columns; i++) {
+        for (let j = 0; j < rows; j++) {
+            if (minas[i][j] == 1) {
+                matrix[i][j].style.backgroundColor = "red";
+                matrix[i][j].style.borderColor = "darkgreen";
+                matrix[i][j].innerText = "";
+                matrix[i][j].removeEventListener("click", minasAlrededor, false); 
+                matrix[i][j].removeEventListener("contextmenu", poneBandera, false); 
+                matrix[i][j].appendChild(bomba());
+            }
+            else {
+                matrix[i][j].style.backgroundColor = "lightgreen";
+                matrix[i][j].style.color = "darkgreen";
+                matrix[i][j].style.borderColor = "darkgreen";
+                matrix[i][j].removeEventListener("click", minasAlrededor, false); 
+                matrix[i][j].removeEventListener("contextmenu", poneBandera, false); 
+            }
+        }
+    }
+}
+
+
+// funcion temporizador de juego
+function temporizador () {
+        tiempo = setInterval(function () { 
+            segundos++;
+            if (segundos == 59) {
+                minutos++;
+                segundos = 0;
+            }
+            if (segundos < 10) {
+                segundos = '0' + segundos; 
+            }
+            document.getElementById("reloj").innerHTML = minutos+':'+segundos;
+            puntuacionGanador = minutos+':'+segundos;
+        },1000);
+}
+
+// funcion para insertar nombre de jugador
+function nombreUsuario() {
+    return new Promise(resolve => {
+        setTimeout(() => { 
+            resolve(prompt("Introduce tu nombre"));
+        });
+    });
+}
+
+// funcion para registar el nombre y puntuacion del ganador
+async function registroGanador () {
+    let celdaNombre = document.createElement("td");
+    let celdaPuntuacion = document.createElement("td");
+    let fila = document.getElementById("ganador");
+    let tabla = document.getElementById("ganadores");
+    celdaNombre.innerHTML = await nombreUsuario();
+    celdaPuntuacion.innerHTML = puntuacionGanador;
+    fila.appendChild(celdaNombre);
+    fila.appendChild(celdaPuntuacion);
+    tabla.style.display = "block";
+
+}
+
+// funcion para resetear el juego
+function reset() {
+    location.reload();
 }
